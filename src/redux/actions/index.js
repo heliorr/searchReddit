babb9433d45b17e71c1508c099d19fa03944ddf8
Helio Souza <heliorr7@gmail.com>
2022-06-20 19:08:56 -0300

    criando store redux

---
+++ src/redux/actions/index.js
@@ -0,0 +1,4 @@
+export const searchReddit = (state) => ({
+    type: 'SEARCH_REDDIT',
+    payload: state,
+});
---
+++ src/redux/reducer/index.js
@@ -0,0 +1,9 @@
+const INITIAL_STATE = {
+    name: '',
+}
+    
+function reducer(state = INITIAL_STATE, action) {
+    return state;
+}
+    
+export default reducer;
